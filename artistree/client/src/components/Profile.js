4ef62256e87e1298e0ec2bc87a2072c8e10206b9
Chantel Taki <chanteltaki@gmail.com>
2020-05-14 13:04:53 -0400

    added handle change etc. to profile

--- artistree/client/src/components/Profile.js
+++ artistree/client/src/components/Profile.js
@@ -17,6 +17,25 @@ export default class Profile extends Component {
   componentDidUpdate = () => {
     this.getData();
   }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+
+  }
+//change pfp
+  handleFileChange(event) {
+    const uploadData = new FormData();
+    uploadData.append('imageUrl', event.target.files[0])
+    uploadData.append('username', response.data.username)
+    console.log(uploadData)
+    //need to change this route
+    axios.post("http://localhost:5000/auth/upload", uploadData)
+    .then(response => this.setState({ imageUrl: response.data.secure_url}))
+    .catch(error => console.log(error))
+  }
   
   getData = () => {
     const id = this.props.match.params.id;
@@ -39,12 +58,25 @@ export default class Profile extends Component {
       }  
     })
   }
+
+  toggleEditForm = () => {
+    this.setState({
+      editForm: !this.state.editForm
+    });
+  }
   
   render() {
+    if (this.state.error) return <div>{this.state.error}</div>
+    if (!this.state.username) return (<></>)
+    let allowedToEdit = false;
+    const user = this.props.user;
+    const owner = this.state.profile.owner;
+    if (user && user._id === owner) allowedToEdit = true;
     return (
       <div>
        <h1>{this.state.username}</h1>
        <img src={this.state.imageUrl} alt="img"/>
+       <button onClick={this.toggleEditForm}>Edit Picture</button>
        <p>{this.state.location}</p>
       </div>
     )
