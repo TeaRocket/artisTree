3f88afb658f556d944dce6de26ebcfe15b15a441
Chantel Taki <chanteltaki@gmail.com>
2020-05-14 05:27:41 -0400

    added methods to call signup, login, logout

---
+++ artistree/client/src/services/auth.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const signup = (username, password, email, birthDate, location, role) => {
+  return axios
+  .post('/api/auth/signup', {username, password, email, birthDate, location, role})
+  .then(response => {
+    return response.data;
+  })
+  .catch(error => {
+    return error.response.data;
+  });
+}
+
+const login = (username, password) => {
+  return axios
+  .post('api/auth/login', {username, password})
+  .then(response => {
+    return response.data;
+  })
+  .catch(error => {
+    return error.response.data;
+  });
+}
+
+const logout = () => {
+  return axios
+  .delete('api/auth/logout')
+  .then(response => {
+    return response.data;
+  })
+  .catch(error => {
+    return error.response.data;
+  });
+}
+
+export {signup, login, logout};
