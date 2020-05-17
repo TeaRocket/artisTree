3136685fd6e7d59fe219f133de6848140370fe5c
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-05-17 20:57:55 +0200

    fixed what gets displays in nav on different pages

--- artistree/client/src/components/Nav/Nav.js
+++ artistree/client/src/components/Nav/Nav.js
@@ -1,39 +1,44 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { UserContext } from "../../contexts/UserContext";
+import { useContext } from "react";
 
-export default class Nav extends Component {
-  static contextType = UserContext;
+export default function Nav() {
+  const { user } = useContext(UserContext);
+  const { pathname, search } = useLocation();
 
-  render() {
-    const { user } = this.context;
-
-    return (
-      <div id="Nav">
-        <div id="nav-inner-l">
-          <Link to="/">
-            <img
-              src={window.location.origin + "/artistree.png"}
-              id="logo"
-              alt="logo"
-            />
-          </Link>
-        </div>
-        <div id="nav-inner-r">
-          <Link to="/">Home</Link>
-          {user ? (
-            <>
-              <Link to="/profile">Profile</Link>
-              <Link to="/logout">Logout</Link>
-            </>
-          ) : (
-            <>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Signup</Link>
-            </>
-          )}
-        </div>
+  const onLoginPage = pathname === "/login";
+  const onSignupPage = pathname === "/signup" && search !== "?artist=true";
+  const onArtistSignupPage = search === "?artist=true";
+  //const onArtistSignup = pathname === "/signup";
+  return (
+    <div id="Nav">
+      <div id="nav-inner-l">
+        <Link to="/">
+          <img
+            src={window.location.origin + "/artistree.png"}
+            id="logo"
+            alt="logo"
+          />
+        </Link>
       </div>
-    );
-  }
+      <div id="nav-inner-r">
+        <Link to="/">Home</Link>
+        {user ? (
+          <>
+            <Link to="/profile">Profile</Link>
+            <Link to="/logout">Logout</Link>
+          </>
+        ) : (
+          <>
+            {!onArtistSignupPage && (
+              <Link to="/signup?artist=true">Create artist profile</Link>
+            )}
+            {!onLoginPage && <Link to="/login">Login</Link>}
+            {!onSignupPage && <Link to="/signup">Signup</Link>}
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
