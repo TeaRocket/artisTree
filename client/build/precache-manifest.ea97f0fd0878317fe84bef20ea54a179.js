2a4d6cdf6cb9f9289910ce073f7c686e240ddcc9
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-05-21 20:13:40 +0200

    msgs done

---
+++ client/build/precache-manifest.ea97f0fd0878317fe84bef20ea54a179.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371076ea642c9524fc41f928aa44a195",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d379d447c54b84122767",
+    "url": "/static/css/2.19b7558e.chunk.css"
+  },
+  {
+    "revision": "7fe472dbb9675a824f99",
+    "url": "/static/css/main.9af28232.chunk.css"
+  },
+  {
+    "revision": "d379d447c54b84122767",
+    "url": "/static/js/2.a166905b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a166905b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe472dbb9675a824f99",
+    "url": "/static/js/main.84ef092d.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
