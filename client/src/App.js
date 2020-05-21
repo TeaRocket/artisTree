560b036769eedb76542463aad5d37c1c3bd2f941
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-05-21 15:18:37 +0200

    added redirect for not-register users

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import axios from "axios";
 import { UserContext } from "./contexts/UserContext";
 import Signup from "./components/Signup/Signup";
@@ -28,23 +28,55 @@ class App extends React.Component {
   }
 
   render() {
+    const { user } = this.context;
+    const RedirectToLogin = ({ history }) => {
+      history.push("/login");
+      return null;
+    };
     return (
       <div className="App">
         <Nav />
-        <Route exact path="/" component={SearchResults} />
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/logout" component={Logout} />
-        <Route exact path="/user/:id" component={Profile} />
-        <Route exact path="/messages" component={MessagesPage} />
-        <Route exact path="/messages/:id" component={MessagesPage} />
-        <Route exact path="/user/:id/artwork" component={AddArtwork} />
-        <Route exact path="/user/:id/artwork" component={ArtworkList} />
-        <Route
-          exact
-          path="/user/:id/artwork/:artworkId"
-          component={ArtworkDetails}
-        />
+        <Switch>
+          <Route exact path="/" component={SearchResults} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/logout" component={Logout} />
+          <Route exact path="/user/:id" component={Profile} />
+          <Route
+            exact
+            path="/user/:id/artwork/:artworkId"
+            component={ArtworkDetails}
+          />
+          {user ? (
+            <>
+              <Route exact path="/messages" component={MessagesPage} />
+              <Route exact path="/messages/:id" component={MessagesPage} />
+              <Route exact path="/user/:id/artwork" component={AddArtwork} />
+              <Route exact path="/user/:id/artwork" component={ArtworkList} />
+            </>
+          ) : (
+            <>
+              <Route exact path="/messages" component={RedirectToLogin} />
+              <Route exact path="/messages/:id" component={RedirectToLogin} />
+              <Route
+                exact
+                path="/user/:id/artwork"
+                component={RedirectToLogin}
+              />
+              <Route
+                exact
+                path="/user/:id/artwork"
+                component={RedirectToLogin}
+              />
+            </>
+          )}
+          <Route
+            component={({ history }) => {
+              history.push("/");
+              return null;
+            }}
+          />
+        </Switch>
       </div>
     );
   }
