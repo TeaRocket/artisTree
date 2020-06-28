6f03719522330295a631ec6bb2803c8cbefe31b9
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-06-28 19:19:55 +0200

    hamburger

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { UserContext } from "../../contexts/UserContext";
 import { useContext } from "react";
@@ -6,10 +6,14 @@ import { useContext } from "react";
 export default function Nav() {
   const { user } = useContext(UserContext);
   const { pathname, search } = useLocation();
-
+  const [checkBoxRef] = useState(React.createRef());
   const onLoginPage = pathname === "/login";
   const onSignupPage = pathname === "/signup" && search !== "?artist=true";
   const onArtistSignupPage = search === "?artist=true";
+
+  const closeMenu = () => {
+    checkBoxRef.current.checked = !checkBoxRef.current.checked;
+  };
   return (
     <nav role="navigation" aria-labelledby="sections-heading">
       <div id="nav-left-container">
@@ -24,33 +28,43 @@ export default function Nav() {
         </div>
 
         <div id="menuToggle">
-          <input type="checkbox" />
+          <input type="checkbox" ref={checkBoxRef} />
           <span></span>
           <span></span>
           <span></span>
           <ul>
             <li className="link">
-              <Link to="/">Home</Link>
+              <Link to="/" onClick={closeMenu}>
+                Home
+              </Link>
             </li>
             {user ? (
               <>
                 <li className="link">
-                  <Link to={`/user/${user._id}`}>Profile</Link>
+                  <Link to={`/user/${user._id}`} onClick={closeMenu}>
+                    Profile
+                  </Link>
                 </li>
                 <li className="link">
-                  <Link to="/logout">Logout</Link>
+                  <Link to="/logout" onClick={closeMenu}>
+                    Logout
+                  </Link>
                 </li>
               </>
             ) : (
               <>
                 {!onArtistSignupPage && (
                   <li className="link">
-                    <Link to="/signup?artist=true">Create artist profile</Link>
+                    <Link to="/signup?artist=true" onClick={closeMenu}>
+                      Create artist profile
+                    </Link>
                   </li>
                 )}
                 {!onLoginPage && (
                   <li className="link">
-                    <Link to="/login">Login</Link>
+                    <Link to="/login" onClick={closeMenu}>
+                      Login
+                    </Link>
                   </li>
                 )}
                 {!onSignupPage && (
@@ -60,6 +74,7 @@ export default function Nav() {
                       className="btn btn-primary"
                       data-toggle="modal"
                       data-target="#exampleModal"
+                      onClick={closeMenu}
                     >
                       Signup
                     </Link>
