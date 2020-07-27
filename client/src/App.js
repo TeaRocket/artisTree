dc76b10137a0e5cffd6d2f93e7ab86d3c88b4afa
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-07-27 13:09:57 +0200

    disconnect isn't working

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import axios from "axios";
 import socketIOClient from "socket.io-client";
--- client/src/components/Logout/Logout.js
+++ client/src/components/Logout/Logout.js
@@ -1,16 +1,24 @@
 import { useContext, useEffect } from "react";
 import { UserContext } from "../../contexts/UserContext";
+import { SocketContext } from "../../contexts/SocketContext";
 import axios from "axios";
 
 export default function Logout(props) {
-  const { setUser } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
+  const { socket, setSocket } = useContext(SocketContext);
 
   useEffect(() => {
+    if (socket) {
+      console.log("disconnect socket io client");
+      socket.emit("disconnect", { userId: user._id });
+      socket.off();
+      setSocket(undefined);
+    }
     axios.delete("/api/auth/logout").then((response) => {
       setUser(null);
       props.history.push("/");
     });
-  }, [props.history, setUser]);
+  }, [props.history, setUser, setSocket, socket]);
 
   return null;
 }
